369d15eaa299247f140ddac368e361f0da74e21b
c0052890Newcastle <c0052890@newcastle.ac.uk>
2022-01-12 13:21:43 +0000

    Add barcode scanner activity and dependency

---
+++ app/src/main/java/com/csc/plastictracker/BarcodeActivity.java
@@ -0,0 +1,14 @@
+package com.csc.plastictracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+    }
+}

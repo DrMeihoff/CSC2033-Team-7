810562393166234aa84f1e1f4f71b1801c41188c
c0052890Newcastle <c0052890@newcastle.ac.uk>
2022-01-13 16:09:01 +0000

    Add handling of button for barcode scanning

--- app/src/main/java/com/csc/plastictracker/MainActivity.java
+++ app/src/main/java/com/csc/plastictracker/MainActivity.java
@@ -1,14 +1,43 @@
 package com.csc.plastictracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA},
+                            50); }
+                else {
+                    openBarcode();
+                }
+            }
+        });
+
+    }
+
+    public void openBarcode() {
+        Intent intent = new Intent(this, BarcodeActivity.class);
+        startActivity(intent);
     }
 }

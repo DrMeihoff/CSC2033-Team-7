966ebf21b4ad1f344ea59e29370a15e7f5081534
c0076020 <c0076020@newcastle.ac.uk>
2022-01-15 19:26:30 +0000

    Add Basic Graph Generation using Temporary Sample Data

--- app/src/main/java/com/csc/plastictracker/GraphActivity.java
+++ app/src/main/java/com/csc/plastictracker/GraphActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.util.Random;
+
 public class GraphActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+
+        GraphView graphView = (GraphView) findViewById(R.id.graphview);
+        graphView.setTitle("Weekly Graph");
+        graphView.getGridLabelRenderer().setVerticalAxisTitle("Amount Recycled (kg)");
+        graphView.getGridLabelRenderer().setHorizontalAxisTitle("Day Of The Week");
+        LineGraphSeries<DataPoint> currentWeek = new LineGraphSeries<DataPoint>(getSampleData());
+        graphView.addSeries(currentWeek);
+    }
+
+
+    /* temporary */
+    protected DataPoint[] getSampleData() {
+        DataPoint[] dataPoints = {
+                new DataPoint(1, new Random().nextDouble()),
+                new DataPoint(2, new Random().nextDouble()),
+                new DataPoint(3, new Random().nextDouble()),
+                new DataPoint(4, new Random().nextDouble()),
+                new DataPoint(5, new Random().nextDouble()),
+                new DataPoint(6, new Random().nextDouble()),
+                new DataPoint(7, new Random().nextDouble()),
+        };
+        return dataPoints;
     }
 }

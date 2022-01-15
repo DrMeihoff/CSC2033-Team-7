bc423c82f5b0b1cf1d93ee2c813095e6528f91c5
c0052890Newcastle <c0052890@newcastle.ac.uk>
2022-01-15 17:11:13 +0000

    Make 1 button do more than one functionality and streamline everything to functions

--- app/src/main/java/com/csc/plastictracker/BarcodeActivity.java
+++ app/src/main/java/com/csc/plastictracker/BarcodeActivity.java
@@ -1,7 +1,5 @@
 package com.csc.plastictracker;
 
-import static android.widget.TextView.BufferType.EDITABLE;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,53 +13,26 @@ import android.widget.Toast;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
 
 public class BarcodeActivity extends AppCompatActivity {
 
     private CodeScanner mCodeScanner;
-
+    private String barcodeId = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
 
-        final EditText barcode_id_edit = findViewById(R.id.barcode_id_edit);
-        final EditText weight_edit = findViewById(R.id.weight_edit);
-        final EditText name_edit = findViewById(R.id.name_edit);
-        final EditText description_edit = findViewById(R.id.description_edit);
-        Button addDbButton = findViewById(R.id.add_db_button);
-        addDbButton.setOnClickListener(v-> {
-            if(TextUtils.isEmpty(barcode_id_edit.getText().toString())
-            || TextUtils.isEmpty(weight_edit.getText().toString())
-            || TextUtils.isEmpty(name_edit.getText().toString())
-            || TextUtils.isEmpty(description_edit.getText().toString())) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(BarcodeActivity.this,"Empty field is not allowed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-            else {
+        initBarcodeScanner();
+    }
 
-                FBRecyclable fbr = new FBRecyclable();
-                Recyclable rec = new Recyclable (
-                        barcode_id_edit.getText().toString(),
-                        Float.valueOf(weight_edit.getText().toString()),
-                        name_edit.getText().toString(),
-                        description_edit.getText().toString()
-                );
-                fbr.add(rec).addOnSuccessListener(suc -> {
-                    Toast.makeText(BarcodeActivity.this,"Added successfully", Toast.LENGTH_SHORT).show();
-                }).addOnFailureListener(er -> {
-                    Toast.makeText(BarcodeActivity.this,"Added unsuccessfully", Toast.LENGTH_SHORT).show();
-                });
-            }
-        });
-
-
-
+    protected void initBarcodeScanner() {
         CodeScannerView scannerView = findViewById(R.id.barcode_view);
         mCodeScanner = new CodeScanner(this, scannerView);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
@@ -71,7 +42,8 @@ public class BarcodeActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(BarcodeActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-                        barcode_id_edit.setText(result.getText(), EDITABLE);
+                        barcodeId = result.getText();
+                        checkIfExists();
                     }
                 });
             }
@@ -80,6 +52,70 @@ public class BarcodeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    protected void setUpButton(boolean check) {
+        EditText weight_edit = findViewById(R.id.weight_edit);
+        EditText name_edit = findViewById(R.id.name_edit);
+        EditText description_edit = findViewById(R.id.description_edit);
+        Button addDbButton = findViewById(R.id.add_db_button);
+        addDbButton.findViewById(R.id.add_db_button);
+        addDbButton.setVisibility(View.VISIBLE);
+        if(check) {
+            addDbButton.setText("Add to database");
+            addDbButton.setOnClickListener(addToDb(weight_edit, name_edit, description_edit));
+        }
+        else {
+            addDbButton.setText("Add");
+            addDbButton.setOnClickListener(addToUser(weight_edit, name_edit, description_edit));
+        }
+    }
+
+    protected View.OnClickListener addToUser(EditText weight_edit, EditText name_edit, EditText description_edit) {
+        //TODO: do this
+        return null;
+    }
+
+    protected View.OnClickListener addToDb(EditText weight_edit, EditText name_edit, EditText description_edit) {
+        if (TextUtils.isEmpty(barcodeId)
+                || TextUtils.isEmpty(weight_edit.getText().toString())
+                || TextUtils.isEmpty(name_edit.getText().toString())
+                || TextUtils.isEmpty(description_edit.getText().toString())) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(BarcodeActivity.this, "Empty field is not allowed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            FBRecyclable fbr = new FBRecyclable();
+            Recyclable rec = new Recyclable(
+                    barcodeId,
+                    Math.round(Float.valueOf(weight_edit.getText().toString()) * 100) / 100,
+                    name_edit.getText().toString(),
+                    description_edit.getText().toString()
+            );
+            fbr.add(rec).addOnSuccessListener(suc -> {
+                Toast.makeText(BarcodeActivity.this, "Added successfully", Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(er -> {
+                Toast.makeText(BarcodeActivity.this, "Added unsuccessfully", Toast.LENGTH_SHORT).show();
+            });
+        }
+        return null;
+    }
+
+    protected void checkIfExists() {
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://plastic-tracker-bfb8b-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("barcodeId");
+        ref.orderByChild("barcodeId").equalTo(barcodeId).addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                setUpButton(dataSnapshot.exists());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }

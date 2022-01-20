8b5a522eda53b85429c644d18d35837a6a07a008
c0076020 <c0076020@newcastle.ac.uk>
2022-01-20 19:58:47 +0000

    Fix GraphActivity So That Results are Shown Based on The Day of The Week

--- app/src/main/java/com/csc/plastictracker/GraphActivity.java
+++ app/src/main/java/com/csc/plastictracker/GraphActivity.java
@@ -1,9 +1,14 @@
 package com.csc.plastictracker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
@@ -12,10 +17,15 @@ import com.jjoe64.graphview.series.DataPoint;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GraphActivity extends AppCompatActivity {
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,28 +33,42 @@ public class GraphActivity extends AppCompatActivity {
 
 
         GraphView graphView = (GraphView) findViewById(R.id.graphview);
-        graphView.setTitle("Weekly Graph");
+        graphView.setTitle("Weekly Graph ");
         graphView.getGridLabelRenderer().setVerticalAxisTitle("Amount Recycled (kg)");
         graphView.getGridLabelRenderer().setHorizontalAxisTitle("Day Of The Week");
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
         staticLabelsFormatter.setHorizontalLabels(new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"});
         graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-        LineGraphSeries<DataPoint> currentWeek = new LineGraphSeries<DataPoint>(getSampleData());
+        LineGraphSeries<DataPoint> currentWeek = new LineGraphSeries<DataPoint>(getData(day()));
         graphView.addSeries(currentWeek);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected String day() {
+        String day = LocalDate.now().getDayOfWeek().name();
+        return day;
+    }
 
-    /* temporary */
-    protected DataPoint[] getSampleData() {
-        DataPoint[] dataPoints = {
-                new DataPoint(1, new Random().nextDouble()),
-                new DataPoint(2, new Random().nextDouble()),
-                new DataPoint(3, new Random().nextDouble()),
-                new DataPoint(4, new Random().nextDouble()),
-                new DataPoint(5, new Random().nextDouble()),
-                new DataPoint(6, new Random().nextDouble()),
-                new DataPoint(7, new Random().nextDouble()),
-        };
+    protected DataPoint[] getData(String day) {
+        ArrayList<String> daysOfWeek = new ArrayList<String>(7);
+        daysOfWeek.add(0, "MONDAY");
+        daysOfWeek.add(1, "TUESDAY");
+        daysOfWeek.add(2, "WEDNESDAY");
+        daysOfWeek.add(3, "THURSDAY");
+        daysOfWeek.add(4, "FRIDAY");
+        daysOfWeek.add(5, "SATURDAY");
+        daysOfWeek.add(6, "SUNDAY");
+        DataPoint[] dataPoints = new DataPoint[7];
+
+        for (String d: daysOfWeek){
+            if(daysOfWeek.indexOf(d) <= daysOfWeek.indexOf(day)) {
+                dataPoints[daysOfWeek.indexOf(d)] = new DataPoint(daysOfWeek.indexOf(d), new Random().nextDouble());
+            }else{
+                dataPoints[daysOfWeek.indexOf(d)] = new DataPoint(daysOfWeek.indexOf(d), 0);
+            }
+        }
+
+
         return dataPoints;
     }
 }
